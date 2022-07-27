66fb955deb6c80ea3fe6f8d6a35022025053a8c8
Andrei Komolov <andreykomolov@gmail.com>
2022-07-27 15:18:31 -0400

    Moved logic from component to custom Hook products.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Product } from './components/Product';
-import { products } from './data/products'
+import React, { useEffect, useState } from "react";
+import { Product } from "./components/Product";
+import { useProducts } from "./hooks/products";
 
 function App() {
+  const { loading, error, products } = useProducts()
+
   return (
-    <div className='container mx-auto max-w-2xl pt-5'>
-
-    { products.map(product => <Product product={product} key={product.id}/>)}
-
-        {/* <Product product={ products[0] }/> */}
-        {/* <Product product={ products[1] }/> */}
+    <div className="container mx-auto max-w-2xl pt-5">
+      {loading && <p className="text-center">Loading...</p>}
+      {error && <p className="text-center text-red-400">{ error }</p>}
+      {products.map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
+      {/* <Product product={ products[0] }/> */}
+      {/* <Product product={ products[1] }/> */}
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/hooks/products.ts
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { IProduct } from "../models";
+import axios, { AxiosError } from "axios";
+
+
+export function useProducts() {
+    const [products, setProducts] = useState<IProduct[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('')
+  
+    async function fetchProducts() {
+      try {
+        setError('')
+        console.log('Loading start')
+        setLoading(true);
+        const response = await axios.get<IProduct[]>(
+          "https://fakestoreapi.com/products?limit=5"
+        );
+        setProducts(response.data);
+        setLoading(false);
+      } catch(e: unknown) {
+        const error = e as AxiosError
+        setLoading(false)
+        setError(error.message)
+      }    
+      
+      console.log('Loading finish')
+    }
+  
+    useEffect(() => {
+      console.log("Effect");
+      fetchProducts();
+    }, []);
+
+    return { products, error, loading }
+}
