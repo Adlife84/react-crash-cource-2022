98d7c018a175f5bff986575e994eaef567b264c2
Andrei Komolov <andreykomolov@gmail.com>
2022-07-28 12:50:11 -0400

    Added button for create product

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,37 @@ import { ErrorMessage } from "./components/ErrorMessage";
 import { Modal } from "./components/Modal";
 import { Product } from "./components/Product";
 import { useProducts } from "./hooks/products";
+import { IProduct } from "./models";
 
 function App() {
-  const { loading, error, products } = useProducts()
+  const { loading, error, products, addProduct } = useProducts();
+  const [modal, setModal] = useState(false);
+
+  const createHandler = (product: IProduct) => {
+    setModal(false);
+    addProduct(product);
+  };
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
       {loading && <p className="text-center">Loading...</p>}
-      {error && <ErrorMessage error={ error } />}
+      {error && <ErrorMessage error={error} />}
       {products.map((product) => (
         <Product product={product} key={product.id} />
       ))}
-      <Modal title="Create new product">
-        <CreateProduct />
-      </Modal>
-      {/* <Product product={ products[0] }/> */}
-      {/* <Product product={ products[1] }/> */}
+
+      {modal && (
+        <Modal title="Create new product" onClose={() => setModal(false)}>
+          <CreateProduct onCreate={createHandler} />
+        </Modal>
+      )}
+
+      <button
+        className="fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"
+        onClick={() => setModal(true)}
+      >
+        +
+      </button>
     </div>
   );
 }
