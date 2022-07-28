397e17559a32fd5a00df05918d630cd6e63d40d7
Andrei Komolov <andreykomolov@gmail.com>
2022-07-28 13:47:34 -0400

    Added Routing from react-router-dom

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,8 @@
+
+export function AboutPage() {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam praesentium error impedit molestias perferendis, exercitationem neque id tempora. Minima voluptatum tenetur tempora consequuntur omnis quidem ullam et a reiciendis quod labore modi doloremque, nulla facilis autem veritatis assumenda magnam nisi itaque suscipit! Quo ea quam sint consequuntur modi porro deleniti.
+        </div>
+    )
+}
